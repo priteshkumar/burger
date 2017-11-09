781f0d36a40aa8fd8d6121b28d6f44e104ef2768
Pritesh <pritesh.kumar2@gmail.com>
2017-11-08 22:08:16 -0800

    jawsdb interface

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ }
+ else{
+  connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "mavpks",
   database: "burgers_db"
 });
+}
 
 connection.connect(function(err) {
   if (err) {
